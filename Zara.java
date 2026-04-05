8e4888c47f0974258473ca2a9ceea56c4dc8dc31
shreyarajkumar07 <shreyarajkumar746@gmail.com>
2026-04-05 12:52:53 +0530

    constuctor chaining concept

---
+++ Zara.java
@@ -0,0 +1,12 @@
+class Zara{
+public static void main(String[]pants){
+	int size=34;
+	float price=2000.000f;
+	long phoneNumber=9562877227888l;
+	double avreage= 54.00000;
+	System.out.println("pants size:"+size);
+	System.out.println("pants price:"+price);
+	System.out.println("pants phoneNumber:"+phoneNumber);
+	System.out.println("pants avreage:"+avreage);
+}
+}
